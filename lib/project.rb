15af33e887fb9fc4942f27d7220dbd00af5af68f
Lara Bjork <lara.m.bjork@gmail.com>
2020-01-10 09:23:21 -0800

    add passing code for Project.save and Project.all

--- lib/project.rb
+++ lib/project.rb
@@ -11,4 +11,20 @@ class Project
     (@title == compare.title)
   end
 
+  def save
+    id = DB.exec("INSERT INTO projects (title) VALUES ('#{@title}') RETURNING id;").first.fetch("id").to_i
+    self
+  end
+
+  def self.all
+    returned_projects = DB.exec("SELECT * FROM projects;")
+    projects = []
+    returned_projects.each() do |project|
+      title = project.fetch("title")
+      id = project.fetch("id").to_i
+      projects.push(Project.new({:title => title, :id => id}))
+    end
+    projects
+  end
+
 end
